d77be9ac5b69221419df83a543175456bdf863fe
muyaho-universe <kimdaesk@gmail.com>
2022-09-02 10:45:11 +0900

    [UPDATE] Main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
+        //Yaho~~~~
         System.out.println("Hello world!");
+        System.out.println("Hello world!!!");
     }
 }
